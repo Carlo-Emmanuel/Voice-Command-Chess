ca6a808572df517a3a35ef0eca956f5e1151fb16
Sarah Armstrong <ghlasphyre@gmail.com>
2023-04-29 18:36:11 -0700

    speech to text working

--- Speech_to_text/setup.py
+++ Speech_to_text/setup.py
@@ -1,9 +1,18 @@
-#import speech_recognition as sr
+import speech_recognition as sr
 import pyaudio
 
-
-
-
 if __name__ == '__main__':
-    print('hi')
-    #p = pyaudio.PyAudio()
+
+    recognizer= sr.Recognizer()
+    mic = sr.Microphone(1)
+    print("Running")
+
+    with mic as source:
+        recognizer.adjust_for_ambient_noise(source, 1)  # Adjust for ambient
+        print("Say something!")
+        audio = recognizer.listen(source)
+    print("Runnnnnn")
+    try:
+        print("Analyzing voice data  " + recognizer.recognize_google(audio, language='en-US'))
+    except Exception:
+        print("Something went wrong")
